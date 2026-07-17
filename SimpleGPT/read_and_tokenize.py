4442590f31112945508042de3a7e1914176e6763
Nihith Wudali <nihithwudali@Nihiths-MacBook-Air.local>
2026-07-18 00:00:13 +0530

    Adding encoding

--- SimpleGPT/read_and_tokenize.py
+++ SimpleGPT/read_and_tokenize.py
@@ -10,8 +10,22 @@ class VocabbingData:
         chars=sorted(list(set(self.input_file)))
         print("Vocab size:", len(chars))
         print("Vocabulary:", ''.join(chars))
+        return chars
     
 test_file_path='lotr.txt'
 lotr_test_file=VocabbingData(test_file_path)
-lotr_test_file.make_set()
+chars=lotr_test_file.make_set()
+
+
+#Encoding karenge basic encoding for now...
+
+character_to_integer={character : integer for integer,character in enumerate(chars)}
+integer_to_character={integer: character for integer,character in enumerate(chars)}
+
+#Now encoding and decoding
+
+encodeded_chars= lambda a : [character_to_integer[c] for c in a]
+decoded_chars= lambda b : ''.join([integer_to_character[i] for i in b])
+
+
 
